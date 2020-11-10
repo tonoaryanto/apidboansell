6acfbc34a8bd4fc50f79dd70ea7aeb7ef53608ae
tonoaryanto <tonoaryanto@gmail.com>
2020-11-10 13:50:40 +0700

    Rev1 bug 8888

--- application/models/Umum_model.php
+++ application/models/Umum_model.php
@@ -58,7 +58,7 @@ class Umum_model extends CI_Model {
 
     function cek_kode($kode,$value,$kategori=null){
         $cek = 0;
-        if($value == 8888){$value == '';}
+        if($value == 8888){$value = NULL;}
         switch ($kode) {
             case "4096":
                 $kode = "req_temp";
