65c5f330a6ef547a753b0d579fd78ca7a922e48f
tonoaryanto <tonoaryanto@gmail.com>
2021-05-18 14:41:06 +0700

    Limit Get

--- application/controllers/Gtwa.php
+++ application/controllers/Gtwa.php
@@ -23,10 +23,18 @@ class Gtwa extends REST_Controller {
         $data = [];
         if($cek_inidata->num_rows() > 0){
             if($kode_kandang == 'all' or $kode_kandang == ''){
-                $rawhouse = $this->db->query("SELECT data_kandang.nama_kandang, data_realtime.date_create, data_realtime.req_temp, data_realtime.avg_temp, data_realtime.temp_1, data_realtime.temp_2, data_realtime.temp_3, data_realtime.temp_4, data_realtime.temp_out, data_realtime.humidity, data_realtime.fan, data_realtime.growday, data_realtime.static_pressure, data_realtime.windspeed, data_realtime.silo1,data_realtime.silo2, data_realtime.feed, data_realtime.water, data_realtime.periode FROM data_realtime LEFT JOIN data_kandang ON data_realtime.kode_kandang = data_kandang.id WHERE data_realtime.kode_perusahaan = '".$datalogin['id_user']."'")->result();
+                $rawhouse = $this->db->query("SELECT data_kandang.urutan, data_kandang.nama_kandang, data_realtime.date_create, data_realtime.req_temp, data_realtime.avg_temp, data_realtime.temp_1, data_realtime.temp_2, data_realtime.temp_3, data_realtime.temp_4, data_realtime.temp_out, data_realtime.humidity, data_realtime.fan, data_realtime.growday, data_realtime.static_pressure, data_realtime.windspeed, data_realtime.silo1,data_realtime.silo2, data_realtime.feed, data_realtime.water, data_realtime.periode FROM data_realtime LEFT JOIN data_kandang ON data_realtime.kode_kandang = data_kandang.id WHERE data_realtime.kode_perusahaan = '".$datalogin['id_user']."' ORDER BY data_kandang.urutan ASC")->result();
                 $house['status'] = 1;
                 $house['data'] = $rawhouse;
-                echo json_encode($house);
+                echo json_encode($house);                
+            }else{
+                $kode_kandang = preg_replace("/[^0-9]/", "", $kode_kandang);
+                if($kode_kandang != ""){
+                    $rawhouse = $this->db->query("SELECT data_kandang.urutan, data_kandang.nama_kandang, data_realtime.date_create, data_realtime.req_temp, data_realtime.avg_temp, data_realtime.temp_1, data_realtime.temp_2, data_realtime.temp_3, data_realtime.temp_4, data_realtime.temp_out, data_realtime.humidity, data_realtime.fan, data_realtime.growday, data_realtime.static_pressure, data_realtime.windspeed, data_realtime.silo1,data_realtime.silo2, data_realtime.feed, data_realtime.water, data_realtime.periode FROM data_realtime LEFT JOIN data_kandang ON data_realtime.kode_kandang = data_kandang.id WHERE data_realtime.kode_perusahaan = '".$datalogin['id_user']."' AND data_kandang.urutan = '".$kode_kandang."'")->result();
+                    $house['status'] = 1;
+                    $house['data'] = $rawhouse;
+                    echo json_encode($house);    
+                }
             }
         }else{
             $house['status'] = false;
